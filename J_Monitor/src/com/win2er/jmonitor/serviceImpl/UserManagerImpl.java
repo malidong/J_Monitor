4c3bcedd20d0b4e7440341aad6f05f8f22558356
malidong <qq125594648@gmail.com>
2015-02-09 23:55:27 +0900

    All right.

--- J_Monitor/src/com/win2er/jmonitor/serviceImpl/UserManagerImpl.java
+++ J_Monitor/src/com/win2er/jmonitor/serviceImpl/UserManagerImpl.java
@@ -13,7 +13,6 @@ import com.win2er.jmonitor.service.UserManager;
 
 public class UserManagerImpl implements UserManager {
 
-	@Override
 	public void regUser(UserBean user) {
 		// TODO Auto-generated method stub
 		
